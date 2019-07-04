12e3a02c25e80cee72eba1ffedfcfa9df0bfa5cd
Jee Ken Chung <chungjeeken@gmail.com>
2019-07-04 21:46:10 +0100

    Introduced foodName prop to FoodItem

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 
-import {Header } from "./Components/Layout";
+import { Header } from "./Components/Layout";
+import FoodItem from "./Components/FoodItem";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <FoodItem foodName={"Chicken Curry"}/>
     </React.Fragment>
   );
 }
--- src/Components/FoodItem.js
+++ src/Components/FoodItem.js
@@ -1,6 +1,18 @@
 import React from "react";
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+const style = {
+    "width": "90%"
+};
 
 export default props => {
-    
+    return (
+        <Paper style={style}>
+            <Typography variant="body1">
+                {props.foodName}
+            </Typography>
+        </Paper>
+    )
 }
 
