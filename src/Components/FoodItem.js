fd3a91f4decbaa447a9768aa25b11de22e39a93d
Jee Ken Chung <chungjeeken@gmail.com>
2019-07-06 19:20:23 +0100

    Positioned IncrementDecrement within FoodItem
    Signed-off-by: Jee Ken Chung <chungjeeken@gmail.com>

--- src/Components/FoodItem.js
+++ src/Components/FoodItem.js
@@ -1,27 +1,53 @@
 import React from "react";
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
 
-import CounterTextField from "./IncrementDecrement/CounterTextField";
+import CounterTextField from "./CounterTextField/CounterTextField";
+import {makeStyles} from "@material-ui/core";
 
-const style = {
-    "width": "90%",
-    "marginTop": "10px",
-    "marginLeft": "auto",
-    "marginBottom": "10px",
-    "marginRight": "auto",
+const useStyles = makeStyles(theme => ({
+    grid: {
+        "flewGrow": 1
+    },
+    paper: {
+        "width": "90%",
+        "marginTop": "10px",
+        "marginLeft": "auto",
+        "marginBottom": "10px",
+        "marginRight": "auto",
 
-    "padding": "5px"
-};
+        "padding": "5px"
+    }
+}));
 
 export default props => {
-    return (
-        <Paper style={style}>
-            <Typography variant="body1">
+    const classes = useStyles();
+
+    return(
+        <Paper className={classes.paper}>
+            <Typography variant="body1" style={{"display": "inline"}}>
                 {props.foodName}
             </Typography>
-            <CounterTextField />
+            <div style={{"float": "right"}}>
+                <CounterTextField />
+            </div>
         </Paper>
     )
+
+    // return (
+    //     <Paper className={classes.paper}>
+    //         <Grid container className={classes.grid} spacing={2}>
+    //             <Grid item xs={10}>
+    //                 <Typography variant="body1">
+    //                     {props.foodName}
+    //                 </Typography>
+    //             </Grid>
+    //             <Grid item xs={2}>
+    //                 <CounterTextField />
+    //             </Grid>
+    //         </Grid>
+    //     </Paper>
+    // )
 }
 
